d0954b9a6b7fd4a10363d83416d37ee78c380e03
Ravindu Perera <ravindurandika2004@gmail.com>
2025-03-10 23:22:45 +0530

    update

---
+++ backend/mosburgerbackend/src/main/java/edu/icet/ecom/entity/Order.java
@@ -0,0 +1,29 @@
+package edu.icet.ecom.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Customer customer;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderItem> items = new ArrayList<>();
+
+    private Double totalAmount;
+    private LocalDateTime timestamp;
+}
